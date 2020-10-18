6e77c48a14dfccb88889021a42619be58751d641
pavani.btech2k9@gmail.com <pkoduru@localhost>
2020-10-18 11:23:07 -0700

    intial commit of hash table program

--- src/main/java/org/nextgen/pavani/THashTable.java
+++ src/main/java/org/nextgen/pavani/THashTable.java
@@ -1,13 +1,22 @@
 package org.nextgen.pavani;
+import java. util.Hashtable;
+import java.util.Set;
+
 
 public class THashTable {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		//TODO:
-		
-
+	Hashtable<String ,Integer> stuDetails=new Hashtable<String,Integer>();
+	stuDetails.put("Dan",1);
+	stuDetails.put("Brad",2);
+	stuDetails.put("Vasim",3);
+	System.out.println(stuDetails);
+	Set<String>key=stuDetails.keySet();
+	for(String names:key) {
+		System.out.println("the names of students are"+names+"roll numbers are"+stuDetails.get(names));
 	}
-
+	
+	
+	}
 }
