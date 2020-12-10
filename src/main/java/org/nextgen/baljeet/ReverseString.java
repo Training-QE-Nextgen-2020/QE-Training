309510a8ab2dc4e4ab56fa8ad9ae36e4979ae3b6
Baljeet <baljeetsohal84@gmail.com>
2020-12-10 10:44:11 -0800

    Reverse String

--- src/main/java/org/nextgen/baljeet/ReverseString.java
+++ src/main/java/org/nextgen/baljeet/ReverseString.java
@@ -4,7 +4,18 @@ public class ReverseString {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		String input = "Welcome to Google!";
+		
+		// Loop Increment or Decrement
+		
+		String output="";
+		
+		for (int index=input.length()-1; index>=0; index--) {
+			
+			output = output + input.charAt(index);
+		}
+		
+		System.out.println(output);
 	}
 
 }
