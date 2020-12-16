c920844e5be297fd37af5122919aabf2e2cfc3b2
Baljeet <baljeetsohal84@gmail.com>
2020-12-16 12:16:09 -0800

    selenium test

---
+++ src/main/java/org/nextgen/baljeet/LaunchApplication.java
@@ -0,0 +1,15 @@
+package org.nextgen.baljeet;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchApplication {
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", ")
+		
+		WebDriver driver = new ChromeDriver(); 
+	}
+
+}
